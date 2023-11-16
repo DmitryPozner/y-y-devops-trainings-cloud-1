e5096b9f31b7738e54b4fa35cb9588f2644324b2
Dmitry Pozner <81529830+DmitryPozner@users.noreply.github.com>
2023-11-16 10:08:00 +0300

    Add files via upload

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,23 +30,36 @@ resource "yandex_container_registry" "catgpt-myregistry" {
 locals {
   folder_id = "b1gudftn90gk7phlf79h"
   service-accounts = toset([
-    "test-acc-2",
+    "catgpt-sa",
+    "catgpt-ig-sa",
   ])
-  test-acc-roles = toset([
+  catgpt-sa-roles = toset([
     "container-registry.images.puller",
     "monitoring.editor",
-    "editor"
   ])
-  service_account_id = "ajermh2idr8msnqtsg1i" 
+  catgpt-ig-sa-roles = toset([
+    "compute.editor",
+    "iam.serviceAccounts.user",
+    "load-balancer.admin",
+    "vpc.publicAdmin",
+    "vpc.user"
+  ])
+
 }
 resource "yandex_iam_service_account" "service-accounts" {
   for_each = local.service-accounts
-  name     = each.key
+  name     = "${local.folder_id}-${each.key}"
 }
-resource "yandex_resourcemanager_folder_iam_member" "test-acc-roles" {
-  for_each  = local.test-acc-roles
+resource "yandex_resourcemanager_folder_iam_member" "catgpt-roles" {
+  for_each  = local.catgpt-sa-roles
   folder_id = local.folder_id
-  member    = "serviceAccount:${yandex_iam_service_account.service-accounts["test-acc-2"].id}"
+  member    = "serviceAccount:${yandex_iam_service_account.service-accounts["catgpt-sa"].id}"
+  role      = each.key
+}
+resource "yandex_resourcemanager_folder_iam_member" "catgpt-ig-roles" {
+  for_each  = local.catgpt-ig-sa-roles
+  folder_id = local.folder_id
+  member    = "serviceAccount:${yandex_iam_service_account.service-accounts["catgpt-ig-sa"].id}"
   role      = each.key
 }
 
@@ -54,10 +67,15 @@ data "yandex_compute_image" "coi" {
   family = "container-optimized-image"
 }
 
-resource "yandex_compute_instance_group" "catgpt-group" {
+resource "yandex_compute_instance_group" "catgpt" {
   name                = "catgpt-ig"
   folder_id           = local.folder_id
-  service_account_id = yandex_iam_service_account.service-accounts["test-acc-2"].id
+  service_account_id = yandex_iam_service_account.service-accounts["catgpt-ig-sa"].id
+  ####adding
+  depends_on = [ 
+    yandex_resourcemanager_folder_iam_member.catgpt-ig-roles
+   ]
+ 
   deletion_protection = false
   instance_template {
     platform_id = "standard-v2"
@@ -74,26 +92,41 @@ resource "yandex_compute_instance_group" "catgpt-group" {
       }
     }
     network_interface {
+      network_id = yandex_vpc_network.foo.id
       subnet_ids = ["${yandex_vpc_subnet.foo.id}"]
       nat = true
     }
-    labels = {
-      label1 = "label1-value"
-      label2 = "label2-value"
-    }
+   
     metadata = {
-      docker-compose = file("${path.module}/docker-compose.yaml")
+      docker-compose = file(
+        "${path.module}/docker-compose.yaml",
+        # {
+        #   folder_id   = "${local.folder_id}",
+        #   registry_id = "${yandex_container_registry.catgpt-myregistry.id}",
+        # }
+      )
+      
       ssh-keys = "ubuntu:${file("~/.ssh/devops_training.pub")}"
     }
+    # временно коммент
     network_settings {
       type = "STANDARD"
     }
+    
   }
 
-  variables = {
-    test_key1 = "test_value1"
-    test_key2 = "test_value2"
+ 
+  load_balancer {
+    target_group_name = "catgpt"
+
   }
+
+  # health_check {
+  #   http_options {
+  #     port = 8080
+  #     path = "/ping"
+  #   }
+  # }
 
   scale_policy {
     fixed_scale {
@@ -106,121 +139,35 @@ resource "yandex_compute_instance_group" "catgpt-group" {
   }
 
   deploy_policy {
-    max_unavailable = 1
-    max_creating    = 1
-    max_expansion   = 1
-    max_deleting    = 1
+    max_unavailable = 2
+    max_creating    = 2
+    max_expansion   = 2
+    max_deleting    = 2
   }
 }
 
-# resource "yandex_compute_instance" "catgpt-1" {
-#     platform_id        = "standard-v2"
-#     service_account_id = yandex_iam_service_account.service-accounts["test-acc-2"].id
-#     resources {
-#       cores         = 2
-#       memory        = 1
-#       core_fraction = 5
-#     }
-#     scheduling_policy {
-#       preemptible = true
-#     }
-#     network_interface {
-#       subnet_id = "${yandex_vpc_subnet.foo.id}"
-#       nat = true
-#     }
-#     boot_disk {
-#       initialize_params {
-#         type = "network-hdd"
-#         size = "30"
-#         image_id = data.yandex_compute_image.coi.id
-#       }
-#     }
-#     metadata = {
-#       docker-compose = file("${path.module}/docker-compose.yaml")
-#       ssh-keys  = "ubuntu:${file("~/.ssh/devops_training.pub")}"
-#     }
-# }
-# resource "yandex_compute_instance" "catgpt-2" {
-#     # platform_id        = "standard-v2"
-#     service_account_id = yandex_iam_service_account.service-accounts["test-acc-2"].id
-#     resources {
-#       cores         = 2
-#       memory        = 1
-#       core_fraction = 5
-#     }
-#     scheduling_policy {
-#       preemptible = true
-#     }
-#     network_interface {
-#       subnet_id = "${yandex_vpc_subnet.foo.id}"
-#       nat = true
-#     }
-#     boot_disk {
-#       initialize_params {
-#         type = "network-hdd"
-#         size = "30"
-#         image_id = data.yandex_compute_image.coi.id
-#       }
-#     }
-#     metadata = {
-#       docker-compose = file("${path.module}/docker-compose.yaml")
-#       ssh-keys  = "ubuntu:${file("~/.ssh/devops_training.pub")}"
-#     }
-# }
+resource "yandex_lb_network_load_balancer" "lb-catgpt" {
+  name = "catgpt"
 
-resource "yandex_lb_target_group" "target_group" {
-  target {
-    subnet_id = "${yandex_vpc_subnet.foo.id}"
-    address = "${yandex_compute_instance_group.catgpt-group.instances[0].network_interface[0].ip_address}"
+  listener {
+    name        = "cat-listener"
+    port        = 80
+    target_port = 8080
+    external_address_spec {
+      ip_version = "ipv4"
+    }
   }
-  # target {
-  #   subnet_id = "${yandex_vpc_subnet.foo.id}"
-  #   address = "${yandex_compute_instance.catgpt-2.network_interface.0.ip_address}"
-  # }
-}
 
-resource "yandex_lb_network_load_balancer" "load-balancer-1" {
-    name         = "load-balancer1"
-    # type         = "internal"
-    deletion_protection = "false"
+  attached_target_group {
+    target_group_id = yandex_compute_instance_group.catgpt.load_balancer[0].target_group_id
 
-    listener {
-      name = "my-listener"
-      port = 8080
-      protocol    = "tcp"
-      internal_address_spec {
-        subnet_id = "${yandex_vpc_subnet.foo.id}"
-    }
-     
-    }
-    
-
-    
-    attached_target_group {
-      target_group_id = "${yandex_lb_target_group.target_group.id}"
-      healthcheck {
-        name = "http"
-          http_options {
-            port = 8080
-            path = "/ping"
-        }
+    healthcheck {
+      name = "http"
+      http_options {
+        port = 8080
+        path = "/ping"
       }
     }
+  }
 }
-
-    
-
-
-
-# resource "yandex_lb_target_group_attachment" "attachment1" {
-#   target_group_id = yandex_lb_target_group.target_group.id
-#   target_id = yandex_compute_instance.catgpt-1.id
-# }
-
-# resource "yandex_lb_target_group_attachment" "attachment2" {
-#   target_group_id = yandex_lb_target_group.target_group.id
-#   target_id = yandex_compute_instance.catgpt-2.id
-# }
-
-#    
 
